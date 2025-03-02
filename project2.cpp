412dcf394d2e744045bd2b9432fc241598303b80
Livia S. Freitas <138071569+livia-freitas@users.noreply.github.com>
2025-03-02 14:08:04 -0600

    Add files via upload

---
+++ project2.cpp
@@ -0,0 +1,29 @@
+/*
+ * CSC-301
+ * project2.cpp
+ * Spring 2025
+ *
+ * Partner 1:
+ * Partner 2:
+ * Date:
+ */
+
+#include "project2.hpp"
+
+using namespace std;
+
+/*
+ * Depth First Search
+ */
+vector<int> DFS(vector<Vertex> &adjList, Vertex &start, Vertex &exit) {
+    vector<int> path;
+    return path;
+}
+
+/*
+ * Breadth First Search
+ */
+vector<int> BFS(vector<Vertex> &adjList, Vertex &start, Vertex &exit) {
+    vector<int> path;
+    return path;
+}
